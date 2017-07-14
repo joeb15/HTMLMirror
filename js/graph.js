ce29bed2ae10e63c1379305b0602753a2cd23997
joeb15 <joebanko15@gmail.com>
2017-07-14 00:39:18 -0700

    Implementation for Viewing graphs

---
+++ js/graph.js
@@ -0,0 +1,17 @@
+/**
+ * Created by joe on 7/14/17.
+ */
+function updateImage(){
+    var canvas = document.getElementById("Graph");
+    canvas.width=720;
+    canvas.height=1280;
+    var ctx = canvas.getContext('2d');
+
+    var sunset = new Image();
+    sunset.src="temperatureGraph.png";
+    sunset.onload = function () {
+        ctx.drawImage(sunset, 0, 0, 720, 360);
+    }
+}
+
+setInterval(updateImage, 30000);
