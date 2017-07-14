37b337f551314a5a743d72eb590df1337455811c
joeb15 <joebanko15@gmail.com>
2017-07-14 01:15:07 -0700

    Just refresh page to save headaches #2

--- js/graph.js
+++ js/graph.js
@@ -1,18 +1,21 @@
 /**
  * Created by joe on 7/14/17.
  */
-function updateImage(){
-    var canvas = document.getElementById("Graph");
 
-    canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
-    var ctx = canvas.getContext('2d');
 
-    var sunset = new Image();
-    sunset.src="temperatureGraph.png";
-    sunset.onload = function () {
-        ctx.drawImage(sunset, 0, 0, window.innerWidth, window.innerWidth/2);
-    }
+var canvas = document.getElementById("Graph");
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+var ctx = canvas.getContext('2d');
+
+var sunset = new Image();
+sunset.src="temperatureGraph.png";
+sunset.onload = function () {
+    ctx.drawImage(sunset, 0, 0, window.innerWidth, window.innerWidth/2);
 }
-updateImage();
-setInterval(location.reload, 10000);
+
+
+setTimeout(function () {
+    location.reload();
+}, 10000);
