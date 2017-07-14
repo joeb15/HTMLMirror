971f249a8fefbef213a7d760a41ea874fdbef16c
joeb15 <joebanko15@gmail.com>
2017-07-14 01:12:04 -0700

    Just refresh page to save headaches

--- js/graph.js
+++ js/graph.js
@@ -9,14 +9,10 @@ function updateImage(){
     var ctx = canvas.getContext('2d');
 
     var sunset = new Image();
-    sunset.src="img/wind.png";
+    sunset.src="temperatureGraph.png";
     sunset.onload = function () {
-        sunset.src="temperatureGraph.png";
-        sunset.onload = function () {
-            ctx.drawImage(sunset, 0, 0, window.innerWidth, window.innerWidth/2);
-        }
+        ctx.drawImage(sunset, 0, 0, window.innerWidth, window.innerWidth/2);
     }
 }
-
 updateImage();
-setInterval(updateImage, 10000);
+setInterval(location.reload, 10000);
