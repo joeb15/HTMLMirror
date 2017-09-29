44444515a2c9de94d7deac97dca81dfc2f7d8a66
Joe Banko <joebanko15@gmail.com>
2017-09-29 00:50:36 -0700

    Make 8 Dates Show instead of 5

--- js/events.js
+++ js/events.js
@@ -46,7 +46,7 @@ function updateEventTimes(events){
     eList.innerHTML="";
     var textContent = document.createElement("td");
     if(events.length>0){
-        for (i = 0; i < events.length && processed.length<5; i++) {
+        for (i = 0; i < events.length && processed.length<8; i++) {
             var event = events[i];
             if(processed.indexOf(event.summary)==-1 && event.summary.indexOf("High")==-1){
                 var month = (event.date.getMonth());
