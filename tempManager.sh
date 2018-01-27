fa07d78a0bb4640e5657e27eb3b5e970fb4f29d5
joeb15 <joebanko15@gmail.com>
2018-01-26 23:04:51 -0800

    temperatureManager

--- tempManager.sh
+++ tempManager.sh
@@ -4,7 +4,10 @@ while true; do
     echo "21" > /sys/class/gpio/export
     echo "out" > /sys/class/gpio/gpio21/direction
 
-    if [ /opt/vc/bin/vcgencmd measure_temp > 50 ]; then
+    cpu=$(</sys/class/thermal/thermal_zone0/temp)
+    temp=$((cpu/1000))
+    echo Temperature is ${temp}
+    if [ temp > 75 ]; then
         echo "1" > /sys/class/gpio/gpio21/value
     else
         echo "0" > /sys/class/gpio/gpio21/value
