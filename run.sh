877b6749e5967d54418e39a91bf3ff8b06cf1a9e
joeb15 <joebanko15@gmail.com>
2018-02-04 22:45:52 -0800

    temperatureManager

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ LOCAL=$(git rev-parse @)
 BASE=$(git merge-base @ "$UPSTREAM")
 
 PID=$(python -m SimpleHTTPServer 8000&)
-trap 'kill $PID' 2
+trap 'kill $PID; exit 1' 2
 
 while true; do
     pull=`git pull`
