99070659e19126d8bc7cebc0a8666507550c9221
joeb15 <joebanko15@gmail.com>
2018-02-04 22:17:17 -0800

    temperatureManager

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ PID=$(python -m SimpleHTTPServer 8000)
 
 while true; do
     pull=`git pull`
-    until [ ${pull} = "Already up-to-date." ]; do
+    echo $pull
+    until [ $pull = "Already up-to-date." ]; do
         sleep 10
     done
     echo Updating
     kill PID
-    PID=$(python -m SimpleHTTPServer 8000)
+    PID=$(python -m SimpleHTTPServer 8000&)
     echo New server running on process ${PID}
 done
