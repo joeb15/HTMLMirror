7a03789615c20e46a07c64bdf66d111cdfd0cf7d
joeb15 <joebanko15@gmail.com>
2018-02-04 23:17:29 -0800

    test

--- runOnUpdate.sh
+++ runOnUpdate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 touch test2
+
+changed=0
+git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+if [ $changed = 1 ]; then
+    git pull
+    echo "Updated successfully";
+else
+    echo "Up-to-date"
+fi
