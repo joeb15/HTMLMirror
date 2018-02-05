f1f0833705a5144d512561273b9221739f336d47
joeb15 <joebanko15@gmail.com>
2018-02-05 14:26:13 -0800

    Display Message Fade Out

--- js/displayMessage.js
+++ js/displayMessage.js
@@ -1,7 +1,18 @@
+var color;
+
 function showMessage(){
+    color = 255;
     document.getElementById("Display Message").style.display="block";
+    document.getElementById("Display Message").style.webkitTextFillColor = getColor(color);
 }
 
-function clearMessage(){
-    document.getElementById("Display Message").style.display="none";
+function clearMessage() {
+    color--;
+    document.getElementById("Display Message").style.webkitTextFillColor = getColor(color);
+}
+
+function getColor(col){
+    if(col<0)
+        col=0;
+    return "rgb("+col+","+col+","+col+")";
 }
