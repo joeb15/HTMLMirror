8655fed85fdfcd0e7e4a427f6ae470735cbbcd75
joeb15 <joebanko15@gmail.com>
2018-02-04 22:03:50 -0800

    test

--- tempManager.sh
+++ tempManager.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 while true; do
-    echo "21" > /sys/class/gpio/export
     echo "out" > /sys/class/gpio/gpio21/direction
 
     cpu=$(</sys/class/thermal/thermal_zone0/temp)
     temp=$((cpu/1000))
     echo Temperature is ${temp}
-    if [ temp > 75 ]; then
+    if [ temp < 75 ]; then
         echo "1" > /sys/class/gpio/gpio21/value
     else
         echo "0" > /sys/class/gpio/gpio21/value
